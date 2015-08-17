ea1426c176fdc0650c1f3bdd8a858781c811c61a
Taylor Huston <taylor.a.huston@gmail.com>
2015-08-17 15:34:02 -0800

    Implement clicking in Window to hide sub menu

--- touchNav.js
+++ touchNav.js
@@ -2,6 +2,13 @@ $('document').ready(function () {
   console.log("loaded");
   $('.hasSub > a').click(function () {
     $(this).toggleClass('clicked');
+    $(this).closest('li').find('ul').toggleClass('visible');
     return !$(this).hasClass('clicked');
+  });
+
+  $(document).click(function() {
+    $('.hasSub > a').removeClass('clicked');
+    $('.subNav').removeClass('visible');
+    console.log("clicked");
   });
 });
